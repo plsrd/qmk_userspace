0b660d5df540063bd5b0967c88174b550f31796b
RD <racheal.larimer@gmail.com>
2025-10-13 13:05:11 -0700

    add more tap dance

--- keyboards/bastardkb/charybdis/4x6/keymaps/plsrd/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/plsrd/keymap.c
@@ -292,5 +292,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return true; // Process all other keycodes normally
   }
 }
-
-,m,,,,,ddddddccccccccccccvvvvkkkkkccccccccccccccccccccccccccc
