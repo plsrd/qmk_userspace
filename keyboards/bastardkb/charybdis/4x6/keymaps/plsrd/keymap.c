c3b105db37e7f26fcdaea64b7a29cd8b2927a7d0
RD <racheal.larimer@gmail.com>
2025-10-17 15:04:39 -0700

    add return statment

--- keyboards/bastardkb/charybdis/4x6/keymaps/plsrd/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/plsrd/keymap.c
@@ -273,24 +273,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
           tap_code16(tap_hold->tap);
       }
-      return false;
+      break;
     case TD(TD_V): // list all tap dance keycodes with tap-hold configurations
       action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
       if (!record->event.pressed && action->state.count && !action->state.finished) {
           tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
           tap_code16(tap_hold->tap);
       }
-      return false;
+      break;
     case TD(TD_K): // list all tap dance keycodes with tap-hold configurations
       action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
       if (!record->event.pressed && action->state.count && !action->state.finished) {
           tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
           tap_code16(tap_hold->tap);
       }
-      return false;
+      break;
     default:
       return true; // Process all other keycodes normally
   }
+  return true
 }
 
 
