8e0f9a26be86d85eb5eed2730575fde7f6b1d760
RD <racheal.larimer@gmail.com>
2025-10-20 12:02:55 -0700

    move drgscrl

--- keyboards/bastardkb/charybdis/4x6/keymaps/plsrd/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/plsrd/keymap.c
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_LSFT,    MT_Z,    MT_X,   TAP_C,   TAP_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_MCTL,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                    MT_ENT, PT_BSPC,  PT_DEL,   KC_ENT,  KC_SPC,
-                                           MS_BTN2,  MS_BTN1,  MT_MCTL
+                                           DRGSCRL,  MS_BTN1,  MT_MCTL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
